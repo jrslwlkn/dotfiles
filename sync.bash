0ad30c7c13068b3d66d7f2f0292548347ec51e3d
Yaroslav <jrslwlkn@gmail.com>
2023-12-16 14:27:32 -0600

    add kitty terminal config

--- sync.bash
+++ sync.bash
@@ -3,10 +3,10 @@
 if [ "$1" = "up" ]; then
 	cp .gitconfig .zshrc .zprofile ~/.
 	cp init.lua ~/.config/nvim/
+	cp kitty.conf ~/.config/kitty/
 	source ~/.zprofile & source ~/.zshrc & wait
 elif [ "$1" = "down" ]; then
-	cp ~/.gitconfig ~/.zshrc ~/.zprofile ./
-	cp ~/.config/nvim/init.lua ./
+	cp ~/.gitconfig ~/.zshrc ~/.zprofile ~/.config/nvim/init.lua ~/.config/kitty/kitty.conf ./
 else 
 	message="Usage:
 	if passed 'up'   -> set system config from repo 
